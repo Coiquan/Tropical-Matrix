0141a217fefaab7016f5755ce530021b2fe4d15f
coiquan <1coiquan@gmail.com>
2022-10-05 23:43:54 +0800

    undone changes

--- Tropical-Matrix/main.cpp
+++ Tropical-Matrix/main.cpp
@@ -1,41 +1,23 @@
 #include "terms.hpp"
+#include <random>
+
+void countergnrt(int *);
+int read(int, vector<terms>);
+int print();
+void remove();
 
 int main()
 {
-    int G2 = G1 + 1;
-    int n2 = 0;
-    for (int n1 = 0; n1 < 10; ++n1)
+    int counter0[8];
+    countergnrt(counter0);
+
+    for (int p0 = 0;;)
     {
         vector<terms> termses; // terms...es?
-        fstream *file0 = new fstream;
-        string name;
-        if (G1 == 0)
-            name = to_string(n1) + ".txt";
-        else
-            name = to_string(G1) + "_" + to_string(n1) + ".txt";
-        name = "../output" + to_string(G1) + "/" + name;
-        file0->open(name, ios::in);
-        if (!file0)
-        {
-            file0->close();
-            delete file0;
-            break;
-        }
-        for (string str1; !file0->eof();)
-        {
-            str1 = "";
-            *file0 >> str1;
-            if (str1 == "")
-                continue;
-            terms *term0 = new terms(str1);
-            termses.push_back(*term0);
-            delete term0;
-        }
-        file0->close();
-        delete file0;
 
-        sort(termses.begin(), termses.end());
-        cout << n1;
+        read(p0, termses);
+
+        int n2 = 0;
 
         long long unsigned i = 1;
         fstream *file1;
@@ -69,5 +51,53 @@ int main()
                 ++n2;
             }
         }
+    }
+    if (p0 == 3)
+        p0 = 1;
+    else
+        ++p0;
+}
+
+void countergnrt(int *cou)
+{
+    int Max = 50000000;
+    int Min = -50000000;
+    random_device seed1;
+    mt19937 gen1(seed1());
+    uniform_int_distribution mm(Min, Max);
+    for (int i = 0; i < 8; ++i)
+    {
+        cou[i] = mm(gen1);
+    }
+}
+
+int read(int p0, vector<terms> termses);
+{
+    for (int n1 = 0;; ++n1)
+    {
+
+        fstream *file0 = new fstream;
+        string name = "../output" + to_string(p0) + "/" + to_string(n1) + ".txt";
+        file0->open(name, ios::in);
+        if (!file0)
+        {
+            file0->close();
+            delete file0;
+            break;
+        }
+        for (string str1; !file0->eof();)
+        {
+            str1 = "";
+            *file0 >> str1;
+            if (str1 == "")
+                continue;
+            terms *term0 = new terms(str1);
+            termses.push_back(*term0);
+            delete term0;
+        }
+        file0->close();
+        delete file0;
+
+        sort(termses.begin(), termses.end());
     }
 }
